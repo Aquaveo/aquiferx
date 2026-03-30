a80f5aea86c441e62f3a2141ff2d62cd57b8f930
romer8 <gio.rombus@gmail.com>
2026-03-30 13:34:48 -0600

    Update build command in vercel.json and set base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -437,6 +437,7 @@ function saveDataPlugin(): Plugin {
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: env.VITE_BASE_PATH || '/',
       server: {
         port: 3000,
         host: '0.0.0.0',
